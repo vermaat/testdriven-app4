72fd460c7980812028be463f94ca5dffbeb7fcc0
Vermaat <mick@vermaat.nl>
2019-01-25 15:38:33 +0800

    Fixed AWS regions
    from us-west to eu-central

--- docker-push.sh
+++ docker-push.sh
@@ -25,9 +25,9 @@ then
     ./awscli-bundle/install -b ~/bin/aws
     export PATH=~/bin:$PATH
     # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
-    eval $(aws ecr get-login --region us-west-1 --no-include-email)
+    eval $(aws ecr get-login --eu-central--1 --no-include-email)
     export TAG=$TRAVIS_BRANCH
-    export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-west-1.amazonaws.com
+    export REPO=$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com
   fi
 
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
